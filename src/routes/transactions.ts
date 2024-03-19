79b01b61c1f95f0a90703c038a3f2f46e92f2648
denisoliveiraa <denis_o@hotmail.com.br>
2024-03-18 22:46:32 -0300

    creat route for get the transactions values and amount values

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import { z } from 'zod'
 import  { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance){
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+    return transactions
+  
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamasSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const {id} = getTransactionParamasSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {transaction}
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions').sum('amount', { as: 'amount'})
+    return summary
+  })
+
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
