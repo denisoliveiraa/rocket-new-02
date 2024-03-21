2f390f9c46ffa803e9feb80d935e71b418f57f2b
denisoliveiraa <denis_o@hotmail.com.br>
2024-03-20 23:21:34 -0300

    New test for get a transaction

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,15 +1,43 @@
- import {expect, test} from 'vitest'
+ import {beforeAll, expect, it, describe} from 'vitest'
  import request from 'supertest'
  import {app} from '../src/app'
 
- test('O usuário consegue criar uma nova transação', async () => {
-  await request(app.server)
 
-  .post('/transactions')
-  .send({
-    title: "Trabalho outros",
-    amount: 6000,
-    type: "debit"
-  })
-  .expect(201)
+ describe('Transaction routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+   })
+  
+   it('O usuário consegue criar uma nova transação', async () => {
+    await request(app.server) // server é uma operação do proprio nodejs vinda de http:node como foi explicado nas primeiras aulas
+  
+    .post('/transactions')
+    .send({
+      title: "Trabalho outros",
+      amount: 6000,
+      type: "debit"
+    })
+    .expect(201)
+   })
+
+   it('Should be able to create a new transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+    .get('/transactions')
+    .set('Cookie', cookies)
+    .expect(200)
+   })
+
  })
+ 
+
+ 
